20f1f46074c3fdacf264fcf40679164bd8fdcb8b
mwnewlin <36387392+mwnewlin@users.noreply.github.com>
2019-05-22 14:59:21 -0400

    Removed comments and added info

--- CSCE 725/rt_identifier.py
+++ CSCE 725/rt_identifier.py
@@ -1,3 +1,7 @@
+# Authors: Marvin Newlin & James Marvin
+# Code Written for CSCE 725 Project, Spring 2019
+# The purpose of this script is to identify the number of Remote Terminals in
+# a traffic dump of MIL-STD-1553 Traffic. 
 
 import pandas as pd
 
@@ -67,43 +71,3 @@ for unique_rt in identified_rt_rt_status_list:
 print("RT Addresses and Message Frequency for RT2RT Status Words")	
 for item in freq_rt2rt_list:
 	print(item)	
-
-
-# Grab Data words following status messages  where RT Address is one we want
-RT_add = 15 #Address of most frequent status message sender
-
-status_and_data = mil_bus_dump.iloc[:,5:]
-data = []
-
-#pert_data = status_and_data[convert_hex_rt_addr(status_and_data.iloc[:,0]) == RT_add]
-
-"""
-for row in status_and_data.index:
-    curr_row = status_and_data.iloc[row]
-    addr = curr_row[0]
-    conv_addr = convert_hex_rt_addr(addr)
-    if conv_addr == RT_add:
-        data.append(curr_row)
-"""	
-# Locate Remote Terminal Addresses in Command Words
-"""
-command_words = mil_bus_dump.iloc[:,3]
-
-command_words_valid = command_words[command_words <= 65536]
-print("First Command Word: ",command_words_valid.iloc[0])
-
-command_words_valid = pd.Series(command_words_valid, dtype=str)
-def get_num_data_words(command_word):
-    bin_word = bin(int(command_word, 16))
-    rt_addr_bin = bin_word[2:5]
-    num_data_words = int(bin_word[11:16],2)
-    print(bin_word)
-    return int(rt_addr_bin,2), num_data_words 
-	
-
-rt,num_words = get_num_data_words(command_words_valid.iloc[0])
-print("Remote Terminal: ",rt)
-print("Number of Data Words: ",num_words)	
-
-	
-"""
